ec2947fb7290716042a65af41785e9cced3e99e2
earowang <earo.wang@gmail.com>
2020-05-01 19:09:29 +1200

    init commit

---
+++ R/formats.R
@@ -0,0 +1,40 @@
+#' Output formats for the University of Auckland documents
+#'
+#' Each function is a wrapper for [`bookdown::pdf_document2()`] to
+#' produce documents in UoA Stats style.
+#'
+#' @param ... Arguments passed to [`bookdown::pdf_document2()`].
+#'
+#' @return An R Markdown output format object.
+#'
+#' @export
+letter <- function(...) {
+  template <- system.file("rmarkdown/templates/Letter/resources/letter.tex",
+                          package = "UOARmdTemplates")
+   bookdown::pdf_document2(..., template = template)
+}
+
+#' @rdname letter
+#' @export
+workingpaper <- function(...) {
+  template <- system.file("rmarkdown/templates/WorkingPaper/resources/wp.tex",
+                          package = "UOARmdTemplates")
+  bookdown::pdf_document2(..., template = template)
+}
+
+
+#' @rdname letter
+#' @export
+report <- function(...) {
+  template <- system.file("rmarkdown/templates/Report/resources/report.tex",
+                          package = "UOARmdTemplates")
+  bookdown::pdf_document2(..., template = template)
+}
+
+#' @rdname letter
+#' @export
+memo <- function(...) {
+  template <- system.file("rmarkdown/templates/Memo/resources/memo.tex",
+                          package = "UOARmdTemplates")
+  bookdown::pdf_document2(..., citation_package = 'biblatex', template = template)
+}
---
+++ README.md
@@ -0,0 +1,23 @@
+# RMarkdown templates for the University of Auckland
+
+This repo is a fork of [robjhyndman/MonashEBSTemplates](https://github.com/robjhyndman/MonashEBSTemplates), but adapted to the University of Auckland for use. [TinyTex](https://yihui.org/tinytex/) is recommended to compile for the pdf.
+
+It contains
+
+* working paper template
+* letter template
+* consulting report template
+* memo template
+
+## Installation
+
+The package can be installed by:
+
+``` r
+# install.packages("remotes")
+remotes::install_github("STATS-UOA/UOARmdTemplates")
+```
+
+## Usage from RStudio
+
+After installation, restart your RStudio then go to File > New File > R Markdown > From Template. Select the template from the list and click OK. It should create a folder (if any external dependencies) and a Rmd file.
